7574b05aeb73f2391af026e38aae63e445de0f51
Dmytro Verner <dmytro.verner@gmail.com>
2018-09-05 02:15:12 +0300

    Solution to 1.2 exercise

---
+++ src/main/java/chapter_1_arrays_and_strings/exercise_2_check_permutation/PermutationChecker.java
@@ -0,0 +1,59 @@
+package chapter_1_arrays_and_strings.exercise_2_check_permutation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PermutationChecker {
+
+    //ask the interviewer whether is case sensitive, whitespace sensitive permutation
+    //ask about the character set
+
+    public boolean isPermutation(String str1, String str2) {
+        return isPermutationAsciiSet(str1, str2);
+    }
+
+    //O(n) time complexity
+    //O(n) space complexity
+    //Unicode character set case
+    private boolean isPermutationUnicodeSet(String str1, String str2) {
+        if(str1 == null || str2 == null)
+            throw new IllegalArgumentException();
+        if(str1.length() != str2.length())
+            return false;
+        Map<Character, Integer> charCountStr1 = new HashMap<>();
+        for(char c : str1.toCharArray()) {
+            charCountStr1.merge(c, 1, (a, b) -> a + b);
+        }
+        for(char c : str2.toCharArray()) {
+            if(charCountStr1.get(c) == null)
+                return false;
+            else
+                charCountStr1.put(c, charCountStr1.get(c) - 1);
+        }
+        for (Integer count : charCountStr1.values()) {
+            if(count != 0)
+                return false;
+        }
+        return true;
+    }
+
+    //O(n) time complexity
+    //O(1) space complexity
+    //ASCII character set case
+    private boolean isPermutationAsciiSet(String str1, String str2) {
+        if(str1 == null || str2 == null)
+            throw new IllegalArgumentException();
+        if(str1.length() != str2.length())
+            return false;
+        int[] set = new int[256];
+        for(Character c : str1.toCharArray()) {
+            set[c]++;
+        }
+        for(Character c : str2.toCharArray()) {
+            if(--set[c] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/test/java/exercise_2_check_permutation/PermutationCheckerTest.java
@@ -0,0 +1,43 @@
+package exercise_2_check_permutation;
+
+import chapter_1_arrays_and_strings.exercise_2_check_permutation.PermutationChecker;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class PermutationCheckerTest {
+
+    private PermutationChecker permutationChecker;
+
+    @Before
+    public void setUp() {
+        permutationChecker = new PermutationChecker();
+    }
+
+    @Test
+    public void permutatedStringsArePermutated() {
+        assertTrue(permutationChecker.isPermutation("abcd", "cbda"));
+    }
+
+    @Test
+    public void notPermutatedStringsAreNotPermutated() {
+        assertFalse(permutationChecker.isPermutation("abcd", "abdd"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwsIAEOnSecondStringBeingNull() {
+        permutationChecker.isPermutation("abc", null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwsIAEOnFirstStringBeingNull() {
+        permutationChecker.isPermutation(null, "abc");
+    }
+
+    @Test
+    public void emptyStringsArePermutatedStrings() {
+        assertTrue(permutationChecker.isPermutation("", ""));
+    }
+}
