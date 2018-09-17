576a6a4bf79b7a121fbb5e7294da93725bc7d536
Dmytro Verner <dmytro.verner@gmail.com>
2018-09-18 00:34:32 +0300

    2 more solutions to 2.4 exercise

--- src/main/java/chapter_2/exercise_4/Partitioner.java
+++ src/main/java/chapter_2/exercise_4/Partitioner.java
@@ -5,7 +5,7 @@ import common.Node;
 public class Partitioner {
 
     public Node partition(Node head, int pivot) {
-        return partitionV1(head, pivot);
+        return partitionV3(head, pivot);
     }
 
     //O(n) time complexity
@@ -42,5 +42,47 @@ public class Partitioner {
             return afterBeg;
         beforeEnd.next = afterBeg;
         return beforeBeg;
+    }
+
+    //O(n) time complexity
+    //O(1) auxiliary space complexity
+    private Node partitionV2(Node node, int pivot) {
+        if (node == null)
+            throw new IllegalArgumentException("Input linked list can't be null");
+        Node head = node;
+        Node tail = node;
+        while (node != null) {
+            Node next = node.next;
+            if (node.data < pivot) {
+                node.next = head;
+                head = node;
+            } else {
+                tail.next = node;
+                tail = node;
+            }
+            node = next;
+        }
+        tail.next = null;
+        return head;
+    }
+
+    //O(n) time complexity
+    //O(1) auxiliary space complexity
+    private Node partitionV3(Node node, int pivot) {
+        if (node == null)
+            throw new IllegalArgumentException("Input linked list can't be null");
+        Node curr = node;
+        Node p = node;
+        while (curr != null) {
+            if (curr.data < pivot) {
+                int temp = p.data;
+                p.data = curr.data;
+                curr.data = temp;
+
+                p = p.next;
+            }
+            curr = curr.next;
+        }
+        return node;
     }
 }
