a249960f9ec7867d7f81319533ceccf48ad400e9
Dmytro Verner <dmytro.verner@gmail.com>
2018-09-28 18:12:45 +0300

    Solution to 2.8

---
+++ src/main/java/chapter_2/exercise_8/LoopDetector.java
@@ -0,0 +1,31 @@
+package chapter_2.exercise_8;
+
+import common.Node;
+
+public class LoopDetector {
+
+    //O(n) time complexity
+    public Node getLoopBeginning(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast)
+                break; //meeting point at abs(LOOP_SIZE - k) in the loop
+        }
+
+        if (fast == null || fast.next == null)
+            return null;
+
+        slow = head;
+        while (slow != fast) {
+            System.out.println(slow.data);
+            System.out.println(fast.data);
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return fast;
+    }
+}
---
+++ src/test/java/chapter_2/LoopDetectorTest.java
@@ -0,0 +1,47 @@
+package chapter_2;
+
+import chapter_2.exercise_8.LoopDetector;
+import common.Node;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class LoopDetectorTest {
+
+    private LoopDetector loopDetector;
+
+    @Before
+    public void setUp() {
+        loopDetector = new LoopDetector();
+    }
+
+    @Test
+    public void detectsLoopBeginningNode() {
+        Node node1 = new Node(new int[] {1, 2, 3});
+        Node node2 = new Node(new int[] {4, 5, 6});
+        node1.next.next.next = node2;
+        node2.next.next.next = node1.next.next;
+
+        assertEquals(3, loopDetector.getLoopBeginning(node1).data);
+    }
+
+    @Test
+    public void detectsLoopBeginningNodeIn2NodesLoop() {
+        Node node1 = new Node(new int[] {1});
+        Node node2 = new Node(new int[] {2});
+        node1.next = node2;
+        node2.next = node1;
+
+        assertEquals(1, loopDetector.getLoopBeginning(node1).data);
+    }
+
+    @Test
+    public void detectsNoLoopInNonLoopingList() {
+        Node node1 = new Node(new int[] {1, 2, 3});
+        node1.next = new Node(new int[] {4, 5, 6, 7});
+
+        assertNull(loopDetector.getLoopBeginning(node1));
+    }
+}
